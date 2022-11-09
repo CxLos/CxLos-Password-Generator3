c78315100d10728a279ce1404dc41f04a4c940d3
cxlos <bautistac_29@outlook.com>
2022-11-09 16:41:39 -0600

    minor tweaks

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var character = ['!', '?', '@', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>']
+var character = ['!', '?', '@', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '[', ']', '{', '}']
 
 //Step 2: Query Selectors 
 var password = document.querySelector('#password')
